baefecb9cc5054bd37cf812d6657e7f75a8bb271
Jorge Antonio Rubén Martínez Martorell <jorgemartinez@MacBook-Pro-de-Jorge.local>
2018-01-15 09:53:38 -0300

    fix credentials classifier

--- controllers/natural-language-classifier.js
+++ controllers/natural-language-classifier.js
@@ -2,12 +2,13 @@
 
 require('dotenv').config()
 const NaturalLanguageClassifierV1 = require('watson-developer-cloud/natural-language-classifier/v1')
+/*
 const natural_language_classifier = new NaturalLanguageClassifierV1({
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     version: process.env.NLC_VERSION_DATE
 })
-
+*/
 function getClassifier (req, res, next) {
     classifier.classify({
         text: req.body.text,
